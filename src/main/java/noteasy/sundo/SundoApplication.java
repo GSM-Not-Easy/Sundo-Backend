a748eaa24f2455036da7c081e4cf7a1ccc3548ef
aesperer <piyrw9754@gmail.com>
2023-11-22 11:20:22 +0900

    create :: 프로젝트 세팅

---
+++ src/main/java/noteasy/sundo/SundoApplication.java
@@ -0,0 +1,13 @@
+package noteasy.sundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noteasy/sundo/SundoApplicationTests.java
@@ -0,0 +1,13 @@
+package noteasy.sundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
