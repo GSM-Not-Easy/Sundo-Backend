34b15643bef37dde4cd41c55be37e592a732f4fb
aesperer <piyrw9754@gmail.com>
2023-11-23 10:36:28 +0900

    create :: custom access handler

---
+++ src/main/java/noteasy/sundo/global/library/filter/JwtFilter.java
@@ -0,0 +1,38 @@
+package noteasy.sundo.global.library.filter;
+
+import lombok.RequiredArgsConstructor;
+import noteasy.sundo.global.library.security.SecurityConfig;
+import noteasy.sundo.global.library.security.token.JwtTokenParser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtTokenParser jwtTokenParser;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = jwtTokenParser.parseAccessToken(request);
+
+        if(!accessToken.isBlank()) {
+            Authentication authentication = jwtTokenParser.authentication(accessToken);
+            SecurityContextHolder.clearContext();
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
