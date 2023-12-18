b741e237df62caaeeb990ac93709adf3e1edc74a
aesperer <piyrw9754@gmail.com>
2023-12-18 16:54:46 +0900

    create :: chat room repository

---
+++ src/main/java/noteasy/sundo/queryfactory/persistmodel/wee/repository/ChatRoomRepository.java
@@ -0,0 +1,7 @@
+package noteasy.sundo.queryfactory.persistmodel.wee.repository;
+
+import noteasy.sundo.queryfactory.persistmodel.wee.ChatRoom;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ChatRoomRepository extends CrudRepository<ChatRoom, Long> {
+}
