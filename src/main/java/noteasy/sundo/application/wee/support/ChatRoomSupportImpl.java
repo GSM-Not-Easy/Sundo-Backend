bd16f55e202bc95fdf1866e36c0f2059df6f556c
aesperer <piyrw9754@gmail.com>
2023-12-18 17:19:21 +0900

    create :: create chat room support

--- src/main/java/noteasy/sundo/application/wee/support/ChatRoomSupportImpl.java
+++ src/main/java/noteasy/sundo/application/wee/support/ChatRoomSupportImpl.java
@@ -9,6 +9,7 @@ import noteasy.sundo.queryfactory.persistmodel.teacher.Teacher;
 import noteasy.sundo.queryfactory.persistmodel.teacher.manager.TeacherRepository;
 import noteasy.sundo.queryfactory.persistmodel.user.User;
 import noteasy.sundo.queryfactory.persistmodel.wee.ChatRoom;
+import noteasy.sundo.queryfactory.persistmodel.wee.repository.ChatMessageRepository;
 import noteasy.sundo.queryfactory.persistmodel.wee.repository.ChatRoomRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -31,8 +32,13 @@ public class ChatRoomSupportImpl implements ChatRoomSupport {
         var wee = teacherRepository.findByStudentAndSubject(Subject.WEE)
                 .orElseThrow(() -> new GlobalException("Not Found Wee Class Teacher..", HttpStatus.NOT_FOUND));
 
-        ChatRoom.builder()
-                .roomName
+        ChatRoom chatRoom = ChatRoom.builder()
+                .chatRoomName(String.format("%s학생과 위클래스 선생님의 채팅방", student.getUser().getName()))
+                .studentId(student.getId())
+                .weeId(wee.getId())
+                .build();
+
+        chatRoomRepository.save(chatRoom);
 
     }
 }
