5021c78257e3c9a5237d8d07aeb37124390c317a
aesperer <piyrw9754@gmail.com>
2023-12-18 17:25:26 +0900

    create :: create chat room api

--- src/main/java/noteasy/sundo/application/wee/executor/WeeExecutor.java
+++ src/main/java/noteasy/sundo/application/wee/executor/WeeExecutor.java
@@ -3,6 +3,7 @@ package noteasy.sundo.application.wee.executor;
 import lombok.RequiredArgsConstructor;
 import noteasy.sundo.application.wee.support.WeeSupport;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -10,7 +11,8 @@ public class WeeExecutor {
 
     private final WeeSupport weeSupport;
 
+    @Transactional(rollbackFor = Exception.class)
     public void createChatRoom() {
-
+        weeSupport.createChatRoom();
     }
 }
