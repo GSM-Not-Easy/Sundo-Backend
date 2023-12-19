fc18a1811c399735e093c3d4938de3cc82d1d234
aesperer <piyrw9754@gmail.com>
2023-12-19 18:04:54 +0900

    application lombok 이건 왜 있어

--- src/main/java/noteasy/sundo/SundoApplication.java
+++ src/main/java/noteasy/sundo/SundoApplication.java
@@ -1,11 +1,9 @@
 package noteasy.sundo;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class SundoApplication {
 
 	public static void main(String[] args) {
