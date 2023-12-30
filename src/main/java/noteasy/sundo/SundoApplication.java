744378d1d7b6ed364a4b450ac4532c91a2097e1a
aesperer <piyrw9754@gmail.com>
2023-12-30 22:46:15 +0900

    exclude 제외

--- src/main/java/noteasy/sundo/SundoApplication.java
+++ src/main/java/noteasy/sundo/SundoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = MongoAutoConfiguration.class)
+@SpringBootApplication
 public class SundoApplication {
 
 	public static void main(String[] args) {
